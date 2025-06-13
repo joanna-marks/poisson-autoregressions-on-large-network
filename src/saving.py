3000a04a103b193cc330aa3e0844d3f2e0edbfa4
joanna-marks <jdm23@ic.ac.uk>
2025-06-13 15:20:53 +0100

    added aux lambda saving

--- src/saving.py
+++ src/saving.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 from datetime import datetime
 
-def save_results(lmbd_comb, lmbd_comb_exp, lmbd_mf,T, comm_size1, comm_size2, prob_matrix, kernel_function, kernel_parameters, experiment_name=None):
+def save_results(lmbd_comb, lmbd_comb_exp, lmbd_mf, lmbd_aux, lmbd_aux_exp, T, comm_size1, comm_size2, prob_matrix, kernel_function, kernel_parameters, experiment_name=None):
 
     # Create a timestamp for unique file identification
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -25,6 +25,8 @@ def save_results(lmbd_comb, lmbd_comb_exp, lmbd_mf,T, comm_size1, comm_size2, pr
     np.save(f"{exp_dir}/lmbd_1000.npy", lmbd_comb)
     np.save(f"{exp_dir}/lmbd_10000.npy", lmbd_comb_exp)
     np.save(f"{exp_dir}/lmbd_mf_1000.npy", lmbd_mf)
+    np.save(f"{exp_dir}/lmbd__aux_1000.npy", lmbd_aux)
+    np.save(f"{exp_dir}/lmbd_aux_10000.npy", lmbd_aux_exp)
 
     # Save experiment metadata
     metadata = {
